422cf6d1516de98e3fa1d1c8ccf69187d739dcff
mrdulin <novaline.dulin@gmail.com>
2021-12-15 09:44:15 +0800

    test: improve example

--- src/stackoverflow/70304592/index.spec.ts
+++ src/stackoverflow/70304592/index.spec.ts
@@ -1,10 +1,11 @@
 describe('70304592', () => {
   it('should pass', () => {
-    const funSpy = jasmine.createSpyObj(['get']);
+    const funSpy = jasmine.createSpyObj('fun', {}, { get: jasmine.createSpy(), value: 'please upvote xD' });
     funSpy.get.and.returnValue('1');
     let serviceSpy = jasmine.createSpyObj(['fun']);
     serviceSpy.fun.and.returnValue(funSpy);
     const x = serviceSpy.fun();
     expect(x.get()).toBe('1');
+    expect(x.value).toBe('please upvote xD');
   });
 });
